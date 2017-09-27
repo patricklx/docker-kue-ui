4bac9f7931a7646bb9305f881831699bf87c21c3
patricklx <patricklx@users.noreply.github.com>
2017-09-27 17:59:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ kue.createQueue({
   redis: {
     createClientFactory() {
       const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
-      return new Redis({sentinels: [redisUrl]);
+      return new Redis({sentinels: [redisUrl]});
     }
   }
 })
