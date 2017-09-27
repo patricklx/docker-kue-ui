e2b6098c4f2923855a68e06dcccb9b0d7c87e49a
patricklx <patricklx@users.noreply.github.com>
2017-09-27 17:34:06 +0200

    support sentinel (#1)
    * support redis sentinel

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const kue = require('kue')
+const Redis = require('ioredis');
 
 kue.createQueue({
   prefix: process.env.KUE_PREFIX,
-  redis: process.env.REDIS_URL || 'redis://redis:6379'
+  redis: {
+    createClientFactory() {
+      const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
+      return new Redis({sentinels: [redisUrl]);
+    }
+  }
 })
 
 kue.app.listen(5000)
